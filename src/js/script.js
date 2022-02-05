78585630c78812a5daaaea025e6e3768cc724a95
Denny Maverick <den_maverick177@mail.ru>
2022-02-05 21:23:18 +0300

    Создание модального окна + javascript

--- src/js/script.js
+++ src/js/script.js
@@ -25,4 +25,70 @@ $(function () {
       });
     }
   });
+
+  // ============ Modal =============
+
+  /*  Modal
+=========================== */
+  let modalCall = $("[data-modal]");
+  let modalClose = $("[data-close]");
+
+  modalCall.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data("modal");
+
+    $(modalId).addClass("show");
+
+    $("body").addClass("no-scroll");
+    //Задержка при анимации модального окна
+    setTimeout(function () {
+      $(modalId).find(".modal__dialog").css({
+        transform: "rotateX(0)",
+      });
+    }, 200);
+
+    // worksSlider.slick("setPosition");
+  });
+
+  modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents(".modal");
+
+    //анимация при закрытии модального окна
+    modalParent.find(".modal__dialog").css({
+      transform: "rotateX(90deg)",
+    });
+
+    //временная задержка
+    setTimeout(function () {
+      modalParent.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+  // Закрытие окна при нажатии на маску
+  $(".modal").on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+
+    $this.find(".modal__dialog").css({
+      transform: "rotateX(90deg)",
+    });
+
+    setTimeout(function () {
+      $this.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  /*отмена события клика родителя элемента (.modal)
+  при клике на блок .modal__dialog окно не будет 
+  закрываться*/
+  $(".modal__dialog").on("click", function (event) {
+    event.stopPropagation();
+  });
 });
