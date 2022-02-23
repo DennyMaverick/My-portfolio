bab2941187d527dd0c04c36ff232cb772cc438cc
Denny Maverick <den_maverick177@mail.ru>
2022-02-23 18:18:18 +0300

    Успешная доработка функции скрыть или показать контент в модальных окнах каждого проекта

--- src/js/modules/content-show-hide.js
+++ src/js/modules/content-show-hide.js
@@ -6,10 +6,11 @@ function informationShow() {
   descriptionShow.forEach(function (item) {
     item.addEventListener("click", function () {
       descriptionHide.forEach(function (item) {
-        item.classList.toggle("hide");
+        item.classList.remove("hide");
       });
       this.classList.add("hide");
 
+      this.nextElementSibling.classList.remove("hide");
       // if (this.nextElementSibling.classList.toggle("hide")) {
       //   this.textContent = "Подробнее";
       // } else {
@@ -22,9 +23,14 @@ function informationShow() {
 function informationHidden() {
   descriptionHide.forEach(function (item) {
     item.addEventListener("click", function () {
-      this.classList.add("hide");
-      this.previousElementSibling.classList.add("hide");
-      descriptionShow.classList.remove('hide');
+      // this.classList.add("hide");
+      this.textContent = "Подробнее";
+      if (this.previousElementSibling.classList.toggle("hide")) {
+        this.textContent = "Подробнее";
+      } else {
+        this.textContent = "Скрыть";
+      }
+      // descriptionShow.classList.remove('hide');
       // descriptionShow.forEach(function (item) {
       //   item.textContent = "Подробнее";
       // });
@@ -33,12 +39,13 @@ function informationHidden() {
 }
 
 function hideEverything() {
+    // item.classList.add("hide");
   descriptionHide.forEach(function (item) {
-    item.classList.add("hide");
+    // item.classList.add("hide");
     item.previousElementSibling.classList.add("hide");
-    // descriptionShow.forEach(function (item) {
-    //   item.textContent = "Подробнее";
-    // });
+    descriptionHide.forEach(function (item) {
+      item.classList.add('hide');
+    });
   });
 }
 
@@ -50,13 +57,19 @@ const closeButtons = document.querySelectorAll(".modal__close");
 const modals = document.querySelectorAll(".modal");
 
 closeButtons.forEach(function (item) {
-  item.addEventListener("click", function () {
+  item.addEventListener("click", function (event) {
+    const closestModal = event.target.closest('.modal');
+    const descriptionShowClosest = closestModal.querySelector('.description-show');
+    descriptionShowClosest.classList.remove('hide');
     hideEverything();
   });
 });
 // При нажатии вне модального окна проекта, скрывать подробную информацию всех проектов
 modals.forEach(function (item) {
-  item.addEventListener("click", function () {
+  item.addEventListener("click", function (event) {
+    const closestModal = event.target.closest('.modal');
+    const descriptionShowClosest = closestModal.querySelector('.description-show');
+    descriptionShowClosest.classList.remove('hide');
     hideEverything();
   });
 });
