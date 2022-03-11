0aa4e3912427644d089b143ea5e69b87b5372043
Denny Maverick <den_maverick177@mail.ru>
2022-03-11 22:21:48 +0300

    добавление контента в модальное окно резюме - сертификаты + убирание бокового скролла при открытии / закрытии модальных окон с сертификатами

--- src/js/modules/hidden-scroll-modal-hire.js
+++ src/js/modules/hidden-scroll-modal-hire.js
@@ -2,14 +2,17 @@ const modalHireMe = document.getElementById('modal-hire-me');
 const modalResume = document.getElementById('modal-resume');
 const body = document.querySelector('body');
 
-const closeBtnHireMe = modalHireMe.querySelector('.modal__close');
+const closeButtonHireMe = modalHireMe.querySelector('.modal__close');
+const closeButtonResume = modalResume.querySelector('.modal__close');
 
-const closeBtnResume = modalResume.querySelector('.modal__close');
+const sertificates = document.querySelectorAll('.sertificate');
 
+const modalSertificates = document.querySelectorAll('.sertificate-modal');
 
 // Убирание скролла с большим приорететом для модального окна с контактами при клике вне модального окна и на кнопку закрыть, если модальное окно resume открыто
 
-closeBtnHireMe.addEventListener('click', () => {
+
+closeButtonHireMe.addEventListener('click', () => {
   if (modalResume.classList.contains('show')) {
     body.classList.add('no-scroll-important');
   }
@@ -21,12 +24,36 @@ modalHireMe.addEventListener('click', () => {
   }
 });
 
+sertificates.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (modalResume.classList.contains('show')) {
+      body.classList.add('no-scroll-important');
+    }
+  });
+});
+
+// sertificate.addEventListener('click', () => {
+//   if (modalResume.classList.contains('show')) {
+//     body.classList.add('no-scroll-important');
+//   }
+// });
+
 
 // Убирание скролла с большим приорететом для модального окна с резюме при клике вне модального окна и на кнопку закрыть
-closeBtnResume.addEventListener('click', () => {
+closeButtonResume.addEventListener('click', () => {
+  body.classList.remove('no-scroll-important');
+});
+
+closeButtonHireMe.addEventListener('click', () => {
   body.classList.remove('no-scroll-important');
 });
 
 modalResume.addEventListener('click', () => {
   body.classList.remove('no-scroll-important');
+});
+
+modalSertificates.forEach((item) => {
+  item.addEventListener('click', () => {
+    body.classList.add('no-scroll-important');
+  });
 });
