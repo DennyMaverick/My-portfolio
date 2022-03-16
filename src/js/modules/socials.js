a6ef4bf9bd0d963e5da80ba4720321cb4dd907c1
Denny Maverick <den_maverick177@mail.ru>
2022-03-16 17:42:37 +0300

    Добавление фото, проект портфолио готов к production

--- src/js/modules/socials.js
+++ src/js/modules/socials.js
@@ -3,9 +3,11 @@ const footerSocials = document.querySelectorAll('.footer__social-image');
 
 socialLink.addEventListener('click', () => {
   footerSocials.forEach((item) => {
+    // item.style.fill = '#363699';
     item.classList.add('footer__social-color-changed');
     setTimeout(function () {
       footerSocials.forEach((item) => {
+        // item.style.fill = '#BBBBCC';
         item.classList.remove('footer__social-color-changed');
       });
     }, 1500);
