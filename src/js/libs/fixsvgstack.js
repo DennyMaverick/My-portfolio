3ad83c8a5703ed8e24eb422eb162042977a90684
Denny Maverick <den_maverick177@mail.ru>
2022-03-28 15:47:59 +0300

    Исправление ошибки в HTML -перезапись полностью файла html

--- src/js/libs/fixsvgstack.js
+++ src/js/libs/fixsvgstack.js
@@ -1,15 +1,18 @@
 (function ($) {
-
   // Setup
   // ----------
 
   // The fix is for webkit browsers only
   // [https://bugs.webkit.org/show_bug.cgi?id=91790]()
 
-  if (!(/WebKit/.test(navigator.userAgent))) {
+  if (!/WebKit/.test(navigator.userAgent)) {
     // return functions that do nothing but support chaining
-    $.fn.fixSVGStack = function () { return this; };
-    $.fn.fixSVGStackBackground = function () { return this; };
+    $.fn.fixSVGStack = function () {
+      return this;
+    };
+    $.fn.fixSVGStackBackground = function () {
+      return this;
+    };
     return;
   }
 
@@ -35,20 +38,18 @@
   // Reads SVG Stack via Ajax and returns one element encoded data-uri.
   var i = 0;
   function getdataURIFromStack(url, cb) {
-
     if (url in cache) {
       cb(cache[url]);
     }
 
     // `url` must be in the form filename.svg#id
-    var parts = url.split('#');
+    var parts = url.split("#");
     if (parts.length !== 2) {
       cb(false);
     }
 
     var processStack = function (xmlText) {
-
-      var xml = (new window.DOMParser()).parseFromString(xmlText, "text/xml")
+      var xml = new window.DOMParser().parseFromString(xmlText, "text/xml");
       // `parts[1]` contains id
       var svg = xml.getElementById(parts[1]);
       if (svg == null) {
@@ -59,17 +60,17 @@
       // Firefox uses viewBox and can't scale SVGs when width and height
       // attributes are defined.
       // Safari on iOS needs width and height to scale properly
-      var viewBox = svg.getAttribute('viewBox');
-      if (viewBox && (viewBoxData = viewBox.split(' ')).length == 4) {
-        svg.setAttribute('width', viewBoxData[2]);
-        svg.setAttribute('height', viewBoxData[3]);
+      var viewBox = svg.getAttribute("viewBox");
+      if (viewBox && (viewBoxData = viewBox.split(" ")).length == 4) {
+        svg.setAttribute("width", viewBoxData[2]);
+        svg.setAttribute("height", viewBoxData[3]);
       }
 
-      var svgString = (new XMLSerializer()).serializeToString(svg);
-      var dataURI = 'data:image/svg+xml;utf-8,' + escape(svgString);
+      var svgString = new XMLSerializer().serializeToString(svg);
+      var dataURI = "data:image/svg+xml;utf-8," + escape(svgString);
       cache[url] = dataURI;
       cb(dataURI);
-    }
+    };
 
     // Ajax request, browser handles caching
     $.ajax({
@@ -77,36 +78,37 @@
       url: parts[0],
       cache: true,
       // Read SVG as 'text', jQuerys XML Parsing is broken with SVGs
-      dataType: 'text',
-      success: processStack
+      dataType: "text",
+      success: processStack,
     });
   }
 
   // Fix for SVG Stacks in background
 
   $.fn.fixSVGStackBackground = function () {
-
     this.each(function () {
-
       var $el = $(this);
 
       // At the heart of the bug:
       // Both jquery's `$el.css('background-image')` and `getComputedStyle($el[0], null).backgroundImage`
       // return and url without the #target part;
 
-      var url = $el[0].style.backgroundImage.slice(4, (- 1)).replace(/["']/g, '');
+      var url = $el[0].style.backgroundImage.slice(4, -1).replace(/["']/g, "");
 
       // Here is the quick and dirty hack, if enabled
 
       if (USE_DIRTY_CSS_CONTENT_HACK) {
         // Read url form `style.content`, the css content property is used to transport the information
         var style = getComputedStyle($el[0], null);
-        if (style.backgroundImage.indexOf('.svg') !== -1 && style.content.indexOf('.svg#') !== -1) {
-          url = style.content.replace(/["']/g, '');
+        if (
+          style.backgroundImage.indexOf(".svg") !== -1 &&
+          style.content.indexOf(".svg#") !== -1
+        ) {
+          url = style.content.replace(/["']/g, "");
         }
       }
 
-      if (url.indexOf('.svg#') === -1) {
+      if (url.indexOf(".svg#") === -1) {
         return;
       }
 
@@ -115,9 +117,8 @@
           return;
         }
         // Replace background-image url with dataURI
-        $el.css('background-image', 'url(' + dataURI + ')');
+        $el.css("background-image", "url(" + dataURI + ")");
       });
-
     });
     return this;
   };
@@ -126,11 +127,10 @@
 
   $.fn.fixSVGStack = function () {
     this.each(function () {
-
       var $el = $(this);
-      var url = $el.attr('src');
+      var url = $el.attr("src");
 
-      if (url.indexOf('.svg#') === -1) {
+      if (url.indexOf(".svg#") === -1) {
         return;
       }
       getdataURIFromStack(url, function (dataURI) {
@@ -138,10 +138,9 @@
           return;
         }
         // Replace src with dataURI
-        $el.attr('src', dataURI);
+        $el.attr("src", dataURI);
       });
     });
     return this;
   };
-
 })(jQuery);
