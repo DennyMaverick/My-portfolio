6c708c4899cc92a5b5724b5eadbdba77cee559dc
Denny Maverick <den_maverick177@mail.ru>
2022-04-05 11:15:57 +0300

    Добавление аккордиона через checkbox в модальных окнах проектов

--- src/js/modules/content-show-modal.js
+++ src/js/modules/content-show-modal.js
@@ -1,9 +1,19 @@
-$(function () {
-  $(window).on("load", function () {
-    $(".show-more").on("click", function (e) {
-      if ($(e.target).hasClass("show-more__trigger")) {
-        $(e.target).next(".show-more__content").slideToggle()
-      }
+const closeBtns = document.querySelectorAll(".theme__close-btn")
+const modals = document.querySelectorAll(".modal")
+const showMoreInputs = document.querySelectorAll(".show-more__input")
+// при клике на крестик убирать checked
+closeBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    showMoreInputs.forEach((input) => {
+      input.checked = false
+    })
+  })
+})
+// при клике вне модального окна убирать checked
+modals.forEach((modal) => {
+  modal.addEventListener("click", () => {
+    showMoreInputs.forEach((input) => {
+      input.checked = false
     })
   })
 })
--- src/js/modules/jQuery/jquery-main.js
+++ src/js/modules/jQuery/jquery-main.js
@@ -3,96 +3,96 @@
 // Libraries
 
 $(function () {
-  const worksSlider = $('[data-slider="slick"]');
+  const worksSlider = $('[data-slider="slick"]')
   /*  Filter
 =========================== */
-  let filter = $("[data-filter]");
+  let filter = $("[data-filter]")
   filter.on("click", function (event) {
-    event.preventDefault(); //убирает стандартное поведение элементов(ссылок)
-    let cat = $(this).data("filter"); //получаем значение элемента, по которому
+    event.preventDefault() //убирает стандартное поведение элементов(ссылок)
+    let cat = $(this).data("filter") //получаем значение элемента, по которому
     //производится клик мыши
 
     if (cat == "all") {
       // $("[data-cat]").slice(0, 6).show();
-      $("[data-cat]").removeClass("hide");
+      $("[data-cat]").removeClass("hide")
     } else {
       $("[data-cat]").each(function () {
-        let workCat = $(this).data("cat");
+        let workCat = $(this).data("cat")
         if (workCat != cat) {
-          $(this).addClass("hide");
+          $(this).addClass("hide")
           // $(this).fadeOut();
         } else {
-          $(this).removeClass("hide");
+          $(this).removeClass("hide")
           // $("[data-cat]").slice(0, 6).show();
         }
-      });
+      })
     }
-  });
+  })
 
   /*  Modal
 =========================== */
-  let modalCall = $("[data-modal]");
-  let modalClose = $("[data-close]");
+  let modalCall = $("[data-modal]")
+  let modalClose = $("[data-close]")
 
   modalCall.on("click", function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    let $this = $(this);
-    let modalId = $this.data("modal");
+    let $this = $(this)
+    let modalId = $this.data("modal")
 
-    $(modalId).addClass("show");
+    $(modalId).addClass("show")
 
-    $("body").addClass("no-scroll");
+    $("body").addClass("no-scroll")
     //Задержка при анимации модального окна
     setTimeout(function () {
       $(modalId).find(".modal__dialog").css({
         transform: "rotateX(0)",
-      });
-    }, 200);
+      })
+    }, 200)
 
-    worksSlider.slick("setPosition");
-  });
+    worksSlider.slick("setPosition")
+  })
 
   modalClose.on("click", function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    let $this = $(this);
-    let modalParent = $this.parents(".modal");
+    let $this = $(this)
+    let modalParent = $this.parents(".modal")
 
     //анимация при закрытии модального окна
     modalParent.find(".modal__dialog").css({
       transform: "rotateX(90deg)",
-    });
+    })
 
     //временная задержка
     setTimeout(function () {
-      modalParent.removeClass("show");
-      $("body").removeClass("no-scroll");
-    }, 200);
-  });
+      modalParent.removeClass("show")
+      $("body").removeClass("no-scroll")
+    }, 200)
+  })
   // Закрытие окна при нажатии на маску
-  $(".modal").on("click", function (event) {
-    event.preventDefault();
 
-    let $this = $(this);
+  $(".modal").on("click", function (event) {
+    event.preventDefault()
+    let $this = $(this)
     if (document.documentElement.clientWidth > 992) {
       $this.find(".modal__dialog").css({
         transform: "rotateX(90deg)",
-      });
+      })
 
       setTimeout(function () {
-        $this.removeClass("show");
-        $("body").removeClass("no-scroll");
-      }, 200);
+        $this.removeClass("show")
+        $("body").removeClass("no-scroll")
+      }, 200)
     }
-  });
+  })
 
   /*отмена события клика родителя элемента (.modal)
   при клике на блок .modal__dialog окно не будет
   закрываться*/
   $(".modal__dialog").on("click", function (event) {
-    event.stopPropagation();
-  });
+    event.stopPropagation()
+  })
 
   // =============== Slider Slick =============
 
@@ -108,31 +108,29 @@ $(function () {
       {
         breakpoint: 769,
         settings: {
-          autoplay: true
-        }
-      }
-    ]
-  });
+          autoplay: true,
+        },
+      },
+    ],
+  })
 
   /*каждой кнопке при нажатии присваиваем метод
 slickPrev(назад) и slickNext(вперед)*/
   $(".slickPrev").on("click", function (event) {
-    event.preventDefault(); //отменяем стандартное поведение кнопок
+    event.preventDefault() //отменяем стандартное поведение кнопок
 
-    let currentSlider = $(this).parents(".modal").find('[data-slider="slick"]');
+    let currentSlider = $(this).parents(".modal").find('[data-slider="slick"]')
     /*в переменной currentSlider хранится слайдер, по которому мы кликаем
   именно в том модальном окне, которое вызвано*/
 
-    currentSlider.slick("slickPrev");
-  });
+    currentSlider.slick("slickPrev")
+  })
   $(".slickNext").on("click", function (event) {
-    event.preventDefault(); //отменяем стандартное поведение кнопок
+    event.preventDefault() //отменяем стандартное поведение кнопок
 
-    let currentSlider = $(this).parents(".modal").find('[data-slider="slick"]');
+    let currentSlider = $(this).parents(".modal").find('[data-slider="slick"]')
     //в переменной currentSlider хранится слайдер, по которому мы кликаем
 
-    currentSlider.slick("slickNext");
-  });
-
-
+    currentSlider.slick("slickNext")
+  })
 })
