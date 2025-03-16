b6583d52e89f5e433a9b601a2c141d1bd7bb458f
Denny Maverick <den_maverick177@mail.ru>
2025-03-17 00:43:51 +0300

    New update

--- src/js/modules/preloader-page.js
+++
@@ -1,9 +0,0 @@
-const preloader = document.getElementById("preloader")
-const bodyEl = document.querySelector("body")
-
-window.onload = function () {
-  if (preloader) {
-    preloader.classList.add("done")
-    bodyEl.classList.remove("no-scroll")
-  }
-}
--- src/js/modules/setCurrentYear.js
+++
@@ -1,7 +0,0 @@
-// Получаем текущий год
-const currentYear = new Date().getFullYear()
-
-// Находим элемент с классом "my-footer-year"
-const footerYear = document.querySelector(".current-year")
-
-footerYear.textContent = currentYear
